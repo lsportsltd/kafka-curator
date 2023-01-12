848b82e2a1a9a39dc3cadb12e2eeedb0c4936f02
Nizan Rosh <Nizan.r@Lsports.eu>
2023-01-12 15:49:25 +0200

    added custom topics file serializer

---
+++ src/KafkaCurator/DefaultTopicsFileSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace KafkaCurator
+{
+    public class DefaultTopicsFileSerializer : ITopicsFileSerializer
+    {
+        public TopicsFile Deserialize(byte[] payload) =>
+            JsonSerializer.Deserialize<TopicsFile>(payload);
+    }
+}
