65277d7f774b4ea7511c43a5a2d8dfdf88a61ae6
Nizan Rosh <Nizan.r@Lsports.eu>
2023-01-12 15:19:14 +0200

    changed to open source kafka curator

---
+++ src/Logic/LSports.Kafka.Curator/Constants/EnvironmentVariableName.cs
@@ -0,0 +1,9 @@
+﻿namespace LSports.Kafka.Curator.Constants
+{
+    public static class EnvironmentVariableName
+    {
+        public static string EnvironmentPrefix = "ASPNETCORE_";
+        public static string HostEnvironment = $"{EnvironmentPrefix}ENVIRONMENT";
+        public static string EnvironmentArgumentName = "env";
+    }
+}
---
+++ src/Logic/LSports.Kafka.Curator/Constants/TopicPattern.cs
@@ -0,0 +1,7 @@
+﻿namespace LSports.Kafka.Curator.Constants
+{
+    public static class TopicPattern
+    {
+        public const string ToExclude = "TopicPrefixesToExclude";
+    }
+}
